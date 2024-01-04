f12c9452907ff02a673310e126f0bc35549528ef
Kris-Ja <107918765+Kris-Ja@users.noreply.github.com>
2024-01-04 21:33:57 +0100

    server

---
+++ server.c
@@ -0,0 +1,168 @@
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+#include<netdb.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/wait.h>
+#include<pthread.h>
+#define MAX_THREADS 5
+#define MAX_CHATS 2
+
+struct client_struct{
+	int cfd;
+	struct sockaddr_in caddr;
+};
+
+static volatile int n_threads = 0, chat_exists[MAX_CHATS+1];
+static volatile char* chat_name[MAX_CHATS+1];
+static volatile int chat_name_length[MAX_CHATS+1];
+static volatile fd_set chat_fd_set[MAX_THREADS+1];
+static pthread_mutex_t n_threads_mutex = PTHREAD_MUTEX_INITIALIZER,
+					   create_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int _write(int fd, char *buf, int len){
+	while (len > 0) {
+		int i = write(fd, buf, len);
+		len -= i;
+		buf += i;
+	}
+}
+
+int _read(int fd, char *buf, int bufsize){
+	int l=0;
+	do {
+		int i = read(fd, buf+l, bufsize);
+		bufsize -= i;
+		l += i;
+	} while (buf[l-1]!='\0' && bufsize>0);
+	printf("l = %d\n", l);
+	return l;
+}
+
+void send_user_join(int chat_id, char* name, int name_length){
+}
+void send_user_leave(int chat_id, char* name, int name_length){
+}
+void send_message(int chat_id, char* name, int name_length, char* buf, int bufsize){
+	for(int i=0; i<=MAX_THREADS; i++){
+		if(FD_ISSET(i, &chat_fd_set[chat_id])){
+			//_write(i, chat_id, 1); // Todo: change chat_id to string
+			_write(i, ": ", 2);
+			_write(i, name, name_length);
+			_write(i, " wrote ", 7);
+			_write(i, buf, bufsize);
+			_write(i, "\n", 1);
+		}
+	}
+}
+void list(int fd){
+	for(int i=0; i<MAX_CHATS; i++){
+		if(chat_exists[i]);// send chat name and id to client
+	}
+}
+
+void join(int chat_id, int fd, char* name, int name_length){
+	FD_SET(fd, &chat_fd_set[chat_id]);
+	send_user_join(chat_id, name, name_length);
+}
+
+void leave(int chat_id, int fd, char* name, int name_length){
+	FD_CLR(fd, &chat_fd_set[chat_id]);
+	send_user_leave(chat_id, name, name_length);
+}
+
+int create(int fd, char* cname, int cname_length){
+	int i;
+	pthread_mutex_lock(&create_mutex); 
+	for(i=0; i<MAX_CHATS; i++){
+		if(!chat_exists[i]){
+			chat_name[i] = cname;
+			chat_name_length[i] = cname_length;
+			chat_exists[i] = 1;
+			break;
+		}
+	}
+	pthread_mutex_unlock(&create_mutex);
+	return i;
+}
+
+void* cthread(void* arg){
+	char buf[300], name[30];
+	int name_length;
+	struct client_struct* client_info = (struct client_struct*)arg;
+	int cfd = client_info->cfd;
+	struct sockaddr_in caddr = client_info->caddr;
+
+	printf("new connection from %s:%d\n", inet_ntoa((struct in_addr)caddr.sin_addr), ntohs(caddr.sin_port));
+	name_length = _read(cfd, name, sizeof(name));
+	join(0, cfd, name, name_length);
+	while(1){
+		int n = _read(cfd, buf, sizeof(buf));
+		int chat_id;
+		switch(buf[0]){
+			case 'L':
+				list(cfd);
+				break;
+			case 'S':
+				chat_id = buf[1]- '0';
+				int i = 2;
+				while(buf[i] != ' ') chat_id = chat_id*10 + buf[i++] - '0';
+				send_message(chat_id, name, name_length, buf+i+1, n-i-1);
+				break;
+			case 'J':
+				chat_id = buf[1] - '0';
+				for(int i=2; i<n-1; i++) chat_id = chat_id*10 + buf[i] - '0';
+				join(chat_id, cfd, name, name_length);
+				break;
+			case 'E':
+				chat_id = buf[1] - '0';
+				for(int i=2; i<n-1; i++) chat_id = chat_id*10 + buf[i] - '0';
+				leave(chat_id, cfd, name, name_length);
+				break;
+			case 'C':
+				chat_id = create(cfd, buf+1, n-1);
+				break;
+		}
+	}
+	// for(int i; i < MAX_CHATS; i++)
+	//	 if(FD_ISSET)leave();
+	close(cfd);
+	free(client_info);
+	
+	pthread_mutex_lock(&n_threads_mutex);
+	n_threads--;
+	pthread_mutex_unlock(&n_threads_mutex);
+	
+	return 0;
+}
+
+int main(int argc, char **argv){
+	socklen_t size;
+	int sfd, on=1;
+	struct sockaddr_in saddr, caddr;
+
+	for(int i=0; i<=MAX_THREADS; i++)FD_ZERO(&chat_fd_set[i]);
+	chat_name[0] = "global";
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	saddr.sin_port = htons(1234);
+	sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
+	bind(sfd, (struct sockaddr*)&saddr, sizeof(saddr));
+	listen(sfd, 10);
+	size = sizeof(caddr);
+
+	while(1){
+		pthread_t tid;
+		struct client_struct* client_info = malloc(sizeof(struct client_struct));
+		while(n_threads >= MAX_THREADS) sleep(1);
+		client_info->cfd = accept(sfd, (struct sockaddr*)&client_info->caddr, &size);
+		pthread_create(&tid, NULL, cthread, client_info);
+		pthread_detach(tid);	
+		pthread_mutex_lock(&n_threads_mutex);
+		n_threads++;
+		pthread_mutex_unlock(&n_threads_mutex);
+	}
+}
