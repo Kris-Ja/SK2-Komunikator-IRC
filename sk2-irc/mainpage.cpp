22445b3ccf135497ac79f4fa9c12857b22187050
anroszkiewicz <111358119+anroszkiewicz@users.noreply.github.com>
2024-01-17 01:46:59 +0100

    client handles channels

--- sk2-irc/mainpage.cpp
+++ sk2-irc/mainpage.cpp
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <cstring>
+#include <vector>
+#include <QListWidget>
+#include <QTextBrowser>
+//#include <QListWidgetItem>
 #include "mainpage.h"
 #include "ui_mainpage.h"
 #include "namechannel.h"
@@ -18,9 +22,25 @@ mainpage::mainpage(SOCKET newfd, QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::mainpage)
 {
+    //initialise channels
+    for(int i=0; i<30; i++)
+    {
+        QListWidget* newListWidget = new QListWidget(this);
+        userLists[i] = newListWidget;
+        userLists[i]->hide();
+
+        QTextBrowser* newTextBrowser = new QTextBrowser(this);
+        chats[i] = newTextBrowser;
+        chats[i]->hide();
+    }
+
     ui->setupUi(this);
     ui->currentChannelLabel->setText("Main channel");
+    ui->channelList->addItem("0 - main channel");
     fd = newfd;
+    currentChat = 0;
+    userLists[0] = ui->userList;
+    chats[0] = ui->textBrowser;
     getUser();
 }
 
@@ -42,7 +62,7 @@ void mainpage::on_sendButton_clicked()
     //read new message from user
     QString newMessage = ui->textEdit->toPlainText();
     //format for sending
-    newMessage = "S0 " + newMessage;
+    newMessage = "S" + QString::number(currentChat) + " " + newMessage;
     //convert to const char*
     QByteArray ba = newMessage.toUtf8();
     const char *toSend = ba.data();
@@ -60,9 +80,61 @@ void mainpage::on_channelButton_clicked()
     nameChannel->show();
 }
 
-void mainpage::onMessageReceived(QString username, QString message)
+void mainpage::onMessageReceived(int chat_id, QString username, QString message)
 {
     QString newMessage = username + ": " + message;
-    ui->textBrowser->append(newMessage);
+    chats[chat_id]->append(newMessage);
+}
+
+void mainpage::onChannelCreated(int chat_id, QString channelName)
+{
+    //display channel name
+    char* stringID;
+    channelName = QString::fromUtf8(itoa(chat_id, stringID, 10)) + " - " + channelName;
+    ui->channelList->insertItem(chat_id, channelName);
+    //QListWidgetItem *newItem = new QListWidgetItem;
+    //newItem->setText(channelName);
+    //ui->channelList->addItem(newItem);
+}
+
+void mainpage::onUserJoined(int chat_id, QString username)
+{
+    //chats[chat_id]->append(username + " joined the chat");
+    userLists[chat_id]->addItem(username);
+}
+
+void mainpage::onChannelDeleted(int chat_id)
+{
+    //clear channel view
+    userLists[chat_id]->clear();
+    chats[chat_id]->clear();
+
+    //delete from channel list
+    QListWidgetItem* deleted = ui->channelList->takeItem(chat_id);
+}
+
+void mainpage::onUserLeft(int chat_id, QString username)
+{
+    chats[chat_id]->append(username + " left the chat");
+    QList<QListWidgetItem *> items = userLists[chat_id]->findItems(username, Qt::MatchExactly);
+    if (items.size() > 0) {
+        int number = userLists[chat_id]->row(items[0]);
+        QListWidgetItem* deleted = userLists[chat_id]->takeItem(number);
+    }
+}
+
+
+void mainpage::on_channelList_itemDoubleClicked(QListWidgetItem *item)
+{
+    ui->currentChannelLabel->setText(item->text());
+    int chat_id = ui->channelList->row(item);
+
+    chats[currentChat]->hide();
+    userLists[currentChat]->hide();
+
+    currentChat = chat_id;
+
+    chats[chat_id]->show();
+    userLists[chat_id]->show();
 }
 
--- sk2-irc/mainpage.h
+++ sk2-irc/mainpage.h
@@ -5,6 +5,9 @@
 #include <winsock2.h>
 #include <windows.h>
 #include <ws2tcpip.h>
+#include <QListWidget>
+#include <QTextBrowser>
+#include <vector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -21,15 +24,24 @@ public:
     ~mainpage();
 
 public slots:
-    void onMessageReceived(QString username, QString message);
+    void onMessageReceived(int chat_id, QString username, QString message);
+    void onChannelCreated(int chat_id, QString channelName);
+    void onUserJoined(int chat_id, QString username);
+    void onChannelDeleted(int chat_id);
+    void onUserLeft(int chat_id, QString username);
 
 private slots:
     void on_sendButton_clicked();
     void on_channelButton_clicked();
 
+    void on_channelList_itemDoubleClicked(QListWidgetItem *item);
+
 private:
     Ui::mainpage *ui;
     void getUser();
     SOCKET fd;
+    int currentChat;
+    QListWidget* userLists[30];
+    QTextBrowser* chats[30];
 };
 #endif // MAINPAGE_H
